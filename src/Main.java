9d0c054110f57f1e6a4243015ae947a4caece06d
pranavvairagade <vairagadepranav@gmail.com>
2024-07-29 23:20:58 +0530

    initial

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Arithmatic operator");
+
+        int a = 15; // datatypes, variable, data value
+        int b = 5; // declaration and initialization
+
+        System.out.println("a+b= " + (a+b)); //addition
+        System.out.println("a-b= " + (a-b)); // sub
+        System.out.println("a*b= " + (a*b)); // multipilcation
+        System.out.println("a/b= " + (a/b)); // devides
+        System.out.println("a%b= " + (a%b)); // gives remiander
+
+
+
+        System.out.println("unary operator");
+        // ++a
+        // a++
+        System.out.println("preincreament : " +  (++a));
+        System.out.println("postincreament : " +  (a++));
+
+        System.out.println("predecreament : " +  (--b));
+        System.out.println("predecreamnet : " +  (b--));
+
+
+
+        }
+    }
