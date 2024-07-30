918b29296420a952948bb73d2d2f1f6dfe0628c2
pranavvairagade <vairagadepranav@gmail.com>
2024-07-31 01:30:07 +0530

    initial

--- src/Main.java
+++ src/Main.java
@@ -14,17 +14,65 @@ public class Main {
         System.out.println("a%b= " + (a%b)); // gives remiander
 
 
-
+        System.out.println();
+        System.out.println("a = "+ a);
         System.out.println("unary operator");
         // ++a
         // a++
-        System.out.println("preincreament : " +  (++a));
-        System.out.println("postincreament : " +  (a++));
+        System.out.println("postincreament : " +  (a++));   //15>16
+        System.out.println("preincreament : " +  (++a));   //16>17
 
-        System.out.println("predecreament : " +  (--b));
-        System.out.println("predecreamnet : " +  (b--));
+        System.out.println("a = "+ a); //17
+
+        System.out.println("predecreamnet : " +  (a--));  //17>16
+        System.out.println("predecreament : " +  (--a));  //16>15
+
+
+        System.out.println();
+
+        System.out.println("Assignment operator");
+
+
+        System.out.println(a); //15
+        System.out.println("a += 4: " + (a += 4));
+        System.out.println("a -= 5: " + (a -= 5));
+        System.out.println("a *= 2: " + (a *= 2));
+        System.out.println("a /= 3: " + (a /= 3));
+        System.out.println("f %= 2: " + (a %= 2));
+
+
+        System.out.println("Relational operator / equality, greater than, and less than");
+
+        int x = 15;
+        int y = 12;
+
+
+        System.out.println("x==y :" + (x==y));
+        System.out.println("x!=y :" + (x!=y));
+        System.out.println("x>y  :" + (x>y));;
+        System.out.println("x<y  :" + (x<y));;
+        System.out.println("x>=y :" + (x>=y));
+        System.out.println("x<=y :" + (x<=y));
+
+        System.out.println("Logical operator");
+
+       //        &&, Logical AND: returns true when both conditions are true.
+        //       ||, Logical OR: returns true if at least one condition is true.
+        //       !, Logical NOT: returns true when a condition is false and vice-versa
+
+        boolean j = true;
+        boolean k = false;
+
+        System.out.println("j && k  : " + (j && k)); // false
+        System.out.println("j || k  : " + (j || k));  // true
+        System.out.println("!j    :"    + (!j));;   // it give opposite
+        System.out.println("!k    :"    + (!k));;
 
 
 
-        }
+        System.out.println("shift operator");
+
+
+
+    }
     }
