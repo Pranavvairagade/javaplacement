95ef7ef814c19eb71a06c2901f6cb2ded89548e4
pranavvairagade <vairagadepranav@gmail.com>
2024-08-25 22:46:34 +0530

    collection hashset example commit

--- src/Collection_Set.java
+++
@@ -1,7 +0,0 @@
-
-import java.util.HashSet;
-public class Collection_Set {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/Collection_Set/HashSetExample.java
@@ -0,0 +1,11 @@
+package Collection_Set;
+import java.util.HashSet;
+public class HashSetExample {
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();
+        set.add("one");
+        System.out.println(set);
+
+    }
+
+}
