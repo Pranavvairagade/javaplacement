5422afa426213543eafa8cddb5a132a993fc3e91
pranavvairagade <vairagadepranav@gmail.com>
2024-08-26 01:21:45 +0530

    collection hashset example commit

---
+++ src/collection_Map/HashMapEmployeeManagement.java
@@ -0,0 +1,21 @@
+package collection_Map;
+
+//Objective: Develop a simple employee management system using HashMap to store and manage employee details.
+//Tasks:
+//Create a HashMap<String, Employee>, where the key is the employee ID and the value is an Employee object.
+//Implement functions to add, update, delete, and retrieve employee details.
+//Implement a feature to find the highest-paid employee.
+//Skills Covered: Object-oriented programming, HashMap for storing complex objects, basic CRUD operations.
+
+public class HashMapEmployeeManagement {
+
+
+
+
+
+
+
+
+
+
+}
